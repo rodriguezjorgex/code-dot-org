69c6c8cf4135ea1691047ef31d7e604ac5334cc8
root <root@DESKTOP-SUAAGI8.localdomain>
2025-02-18 16:16:03 -0500

    add gate to allow students to login or signup when logged out joining a section

--- apps/src/simpleSignUp/lti/link/LtiLinkAccountPage/LtiWelcomeBanner/index.tsx
+++ apps/src/simpleSignUp/lti/link/LtiLinkAccountPage/LtiWelcomeBanner/index.tsx
@@ -2,7 +2,7 @@ import FontAwesomeV6Icon from '@code-dot-org/component-library/fontAwesomeV6Icon
 import React, {useCallback, useContext} from 'react';
 
 import Typography from '@cdo/apps/componentLibrary/typography/Typography';
-import codeLogo from '@cdo/apps/templates/images/codeLogo.svg';
+import codeLogo from '@cdo/apps/templates/images/codeLogo.png';
 import i18n from '@cdo/locale';
 
 import canvas from '../../../assets/canvas.svg';
