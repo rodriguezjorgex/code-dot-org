51cb0225be355527614582dce4f3644bcb379fa6
denyslevada <levada.denys@gmail.com>
2025-03-10 12:12:48 +0200

    chore(faqAccordion): added FAQAccordionItem type export, added story with multiple paragraph

--- frontend/packages/component-library/src/accordion/faqAccordion/index.ts
+++ frontend/packages/component-library/src/accordion/faqAccordion/index.ts
@@ -1,2 +1,6 @@
-export {default as FAQAccordion, FAQAccordionProps} from './FAQAccordion';
+export {
+  default as FAQAccordion,
+  FAQAccordionProps,
+  FAQAccordionItem,
+} from './FAQAccordion';
 export {default as default} from './FAQAccordion';
