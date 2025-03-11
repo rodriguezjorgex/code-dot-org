27beae907802cb2e16bf49be95f09f2965f7fb84
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-03-11 10:12:46 -0700

    Revert "Merge pull request #64456 from code-dot-org/stephen/fa-missing"
    This reverts commit f526a3b8b7285bb21d366f0232a175c23576a4f9, reversing
    changes made to fa98c4b3324f6ab404c926d9033987d2f626b207.

--- frontend/apps/marketing/src/app/layout.tsx
+++ frontend/apps/marketing/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import {GoogleAnalytics} from '@next/third-parties/google';
 import type {Metadata} from 'next';
 
 import '@code-dot-org/component-library-styles/font-awesome.scss';
@@ -5,8 +6,6 @@ import '@code-dot-org/component-library-styles/colors.scss';
 import '@code-dot-org/fonts/index.css';
 
 import './globals.css';
-
-import {GoogleAnalytics} from '@next/third-parties/google';
 import {headers} from 'next/headers';
 import {getGoogleAnalyticsMeasurementId} from '@/config/ga4';
 import {getBrandFromHostname} from '@/config/brand';
