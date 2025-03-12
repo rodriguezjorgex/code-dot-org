f3a7eb9ef9166788e16ea2ddcac9204453f086a5
Erin Bond <erin.bond@code.org>
2025-03-12 10:45:15 -0400

    fix typo

--- dashboard/app/helpers/ai_system_prompts/evaluate_system_prompt_helper.rb
+++ dashboard/app/helpers/ai_system_prompts/evaluate_system_prompt_helper.rb
@@ -5,7 +5,7 @@ module AiSystemPrompts::EvaluateSystemPromptHelper
       Please review the student's work. Respond in correctly formatted JSON.
       evaluationCriteria should just be a copy of #{evaluation_criteria}.
       aiEvaluation should be your assessment of the student's work. Respond with "great", "ok", or "needs revision".
-      aiReasoning should be rovide one sentence with your reasoning.
+      aiReasoning should be one sentence with your reasoning.
     TEXT
     prompt = AiSystemPrompts::SystemPromptHelper.get_basic_system_prompt(level, unit)
     prompt << evaluation_structure
