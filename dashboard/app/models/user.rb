5da9ef0ac1675ee96ca99935217af1575e5c1b2e
Juan Manzo <juan.manzo@code.org>
2025-03-18 14:06:48 -0700

    Reorder macros

--- dashboard/app/models/user.rb
+++ dashboard/app/models/user.rb
@@ -372,12 +372,12 @@ class User < ApplicationRecord
   end
 
   # Username validations
+  before_validation :generate_username, on: :create
   validates_length_of :username, within: 5..20, allow_blank: true
   validates_format_of :username, if: :username_changed?, with: USERNAME_REGEX, allow_blank: true
   validates_uniqueness_of :username, allow_blank: true, case_sensitive: false, on: :create, if: -> {errors.blank?}
   validates_uniqueness_of :username, case_sensitive: false, on: :update, if: -> {errors.blank? && username_changed?}
   validates_presence_of :username, if: :username_required?
-  before_validation :generate_username, on: :create
 
   validates_presence_of :user_type
   validates_inclusion_of :user_type, in: USER_TYPE_OPTIONS, if: :user_type?
