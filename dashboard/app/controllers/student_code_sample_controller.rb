99709c2c67c3d33865a8678479ee3cb7f5df57c0
Erin Bond <erin.bond@code.org>
2025-03-17 20:36:11 -0400

    add evalutions to stuent code samples in Student Dataset Maker

--- dashboard/app/controllers/student_code_sample_controller.rb
+++ dashboard/app/controllers/student_code_sample_controller.rb
@@ -3,45 +3,32 @@ class StudentCodeSampleController < ApplicationController
 
   # GET /student_code_sample/fetch_student_code_samples
   def fetch_student_code_samples
-    level_id = params[:level_id]
-    script_id = params[:script_id]
-    num_samples = params[:num_samples].to_i
+    level_id = student_code_sample_params[:level_id]
+    unit_id = student_code_sample_params[:unit_id]
+    num_samples = student_code_sample_params[:num_samples].to_i
 
     return render json: [] if num_samples == 0
+    check_valid_level(level_id)
+    check_valid_unit(unit_id)
 
-    begin
-      Level.find(level_id)
-    rescue ActiveRecord::RecordNotFound
-      return render status: :not_found, json: "Level with id #{level_id}"
+    if student_code_sample_params[:include_ai_evaluations]
+      fetch_student_code_samples_with_evaluations(level_id, unit_id, num_samples)
+    else
+      fetch_student_code_samples_without_evaluations(level_id, unit_id, num_samples)
     end
+  end
 
-    begin
-      Unit.find(script_id)
-    rescue ActiveRecord::RecordNotFound
-      return render status: :not_found, json: "Script with id #{script_id}"
-    end
-
-    s3 = Aws::S3::Client.new
-    bucket = CDO.sources_s3_bucket
-    base_dir = CDO.sources_s3_directory
+  def fetch_student_code_samples_without_evaluations(level_id, unit_id, num_samples)
     # We want to pull samples from students who have been assigned to work on the level.
-    sections = Section.where(script_id: params[:script_id].to_i)
-    student_ids = include_evaluations ? evaluations.pluck(:user_id) : Follower.where(section: sections).pluck(:student_user_id)
+    sections = Section.where(script_id: unit_id)
+    student_ids = Follower.where(section: sections).pluck(:student_user_id)
     code_samples = []
     have_enough_samples = false
     student_ids.each do |student_id|
       unless have_enough_samples
-        storage_id = storage_id_for_user_id(student_id)
-        channel_token = ChannelToken.where(storage_id: storage_id, level_id: level_id, script_id: script_id).last
-        user_level = UserLevel.where(user_id: student_id, level_id: level_id, script_id: script_id).last
-        if user_level && channel_token
-          storage_app_id = channel_token.storage_app_id
-          token = storage_encrypt_channel_id(storage_id, storage_app_id)
-          _owner_storage_id, project_id = storage_decrypt_channel_id(token)
-          s3_filename = "#{base_dir}/#{storage_id}/#{project_id}/main.json"
-          body = s3.get_object(bucket: bucket, key: s3_filename)[:body].read
-          student_code = JSON.parse(body)['source'] if body
-          code_samples << {level_id: level_id, script_id: script_id, user_id: student_id, project_id: token, student_code: student_code}
+        student_code = get_student_code(student_id, level_id, unit_id)
+        if student_code[:student_code]
+          code_samples << {level_id: level_id, unit_id: unit_id, user_id: student_id, project_id: student_code[:project_id], student_code: student_code[:student_code]}
         end
         have_enough_samples = code_samples.length >= num_samples
       end
@@ -49,65 +36,77 @@ class StudentCodeSampleController < ApplicationController
     render json: code_samples
   end
 
-  # GET /student_code_sample/fetch_evaluated_student_code_samples
-  def fetch_evaluted_student_code_samples
-    level_id = params[:level_id]
-    script_id = params[:script_id]
-    num_samples = params[:num_samples].to_i
-
-    return render json: [] if num_samples == 0
-
-    begin
-      Level.find(level_id)
-    rescue ActiveRecord::RecordNotFound
-      return render status: :not_found, json: "Level with id #{level_id}"
-    end
-
-    begin
-      Unit.find(script_id)
-    rescue ActiveRecord::RecordNotFound
-      return render status: :not_found, json: "Script with id #{script_id}"
-    end
-
-    evaluations = UserLevelEvaluation.where(level_id: level_id, script_id: script_id)
+  def fetch_student_code_samples_with_evaluations(level_id, unit_id, num_samples)
+    evaluations = UserLevelEvaluation.where(level_id: level_id, script_id: unit_id)
     if evaluations.empty?
-      return render status: :not_found, json: "There are no evaluations for the level with id #{level_id} in script with id #{script_id}"
+      return render status: :not_found, json: "There are no evaluations for the level with id #{level_id} in unit with id #{unit_id}"
     end
-
-    s3 = Aws::S3::Client.new
-    bucket = CDO.sources_s3_bucket
-    base_dir = CDO.sources_s3_directory
-
     code_samples = []
     have_enough_samples = false
     evaluations.each do |evaluation|
-      student_id = evaluation.user_id
       unless have_enough_samples
-        storage_id = storage_id_for_user_id(student_id)
-        channel_token = ChannelToken.where(storage_id: storage_id, level_id: level_id, script_id: script_id).last
-        user_level = UserLevel.where(user_id: student_id, level_id: level_id, script_id: script_id).last
-        if user_level && channel_token
-          storage_app_id = channel_token.storage_app_id
-          token = storage_encrypt_channel_id(storage_id, storage_app_id)
-          _owner_storage_id, project_id = storage_decrypt_channel_id(token)
-          # TODO: figure out if this is the correct way to get the code from the s3 bucket if we have a version.
-          s3_filename = "#{base_dir}/#{storage_id}/#{project_id}/main.json?version=#{evaluation.code_version}`"
-          body = s3.get_object(bucket: bucket, key: s3_filename)[:body].read
-          student_code = JSON.parse(body)['source'] if body
+        student_code = get_student_code(evaluation.user_id, level_id, unit_id, evaluation.code_version)
+        if student_code[:student_code]
           code_samples << {
             level_id: level_id,
-            script_id: script_id,
-            user_id: student_id,
-            project_id: token,
-            student_code: student_code,
-            evaluation: evaluation.aiEvaluation,
-            reasoning: evaluation.aiReasoning,
-            criteria: evaluation.evaluationCriteria
+            unit_id: unit_id,
+            user_id: evaluation.user_id,
+            project_id: student_code[:project_id],
+            code_version: student_code[:code_version],
+            student_code: student_code[:student_code],
+            evaluation: evaluation.ai_evaluation,
+            reasoning: evaluation.ai_reasoning,
+            criteria: evaluation.evaluation_criteria
           }
         end
         have_enough_samples = code_samples.length >= num_samples
       end
     end
     render json: code_samples
+  end
+
+  def get_student_code(user_id, level_id, unit_id, code_version = nil)
+    s3 = Aws::S3::Client.new
+    bucket = CDO.sources_s3_bucket
+    base_dir = CDO.sources_s3_directory
+
+    storage_id = storage_id_for_user_id(user_id)
+    channel_token = ChannelToken.where(storage_id: storage_id, level_id: level_id, script_id: unit_id).last
+    user_level = UserLevel.where(user_id: user_id, level_id: level_id, script_id: unit_id).last
+    if user_level && channel_token
+      storage_app_id = channel_token.storage_app_id
+      token = storage_encrypt_channel_id(storage_id, storage_app_id)
+      _owner_storage_id, project_id = storage_decrypt_channel_id(token)
+      s3_filename = "#{base_dir}/#{storage_id}/#{project_id}/main.json"
+      body = s3.get_object(bucket: bucket, key: s3_filename, version_id: code_version)[:body].read
+      student_code = JSON.parse(body)['source'] if body
+    end
+    {
+      project_id: token,
+      code_version: code_version,
+      student_code: student_code,
+    }
+  end
+
+  def check_valid_level(level_id)
+    Level.find(level_id)
+  rescue ActiveRecord::RecordNotFound
+    return render status: :not_found, json: "Level with id #{level_id}"
+  end
+
+  def check_valid_unit(unit_id)
+    Unit.find(unit_id)
+  rescue ActiveRecord::RecordNotFound
+    return render status: :not_found, json: "Unit with id #{unit_id}"
+  end
+
+  def student_code_sample_params
+    student_code_sample_params = params.transform_keys(&:underscore).permit(
+      :level_id,
+      :unit_id,
+      :num_samples,
+      :include_ai_evaluations,
+    )
+    student_code_sample_params
   end
 end
