956f86d9ca23ca276e9ae927a28aabcd0508c035
Liam Frye-Mason <liam.fryemason@code.org>
2025-03-28 07:46:45 -0700

    [HOME] Add an empty state if there are no sections to display  (#64477)
    * First try
    
    * Fix BE routing
    
    * Add test for archive_all endpoint
    
    * Add modal
    
    * Unit tests
    
    * fix test
    
    * Fix test
    
    * fix test 2
    
    * Small changes
    
    * Fix tests
    
    * Archive all
    
    * Move the header to it's own class
    
    * Add logic to show
    
    * Comment
    
    # Conflicts:
    #	apps/src/templates/studioHomepages/teacherHomepageV2/TeacherHomepage.tsx
    
    * Style empty page
    
    * 18n
    
    * Add test
    
    * Fix test

--- apps/src/templates/studioHomepages/teacherHomepageV2/TeacherHomepage.tsx
+++ apps/src/templates/studioHomepages/teacherHomepageV2/TeacherHomepage.tsx
@@ -6,6 +6,7 @@ import i18n from '@cdo/locale';
 
 import {asyncLoadTeacherHomepageSectionData} from '../../teacherDashboard/teacherSectionsRedux';
 
+import {EmptyHomepage} from './EmptyHomepage';
 import {Header} from './Header';
 import {SectionList} from './SectionList';
 
@@ -25,6 +26,19 @@ export const TeacherHomepage: React.FC = () => {
   const [selectedArchiveToggle, setSelectedArchiveToggle] =
     React.useState<ArchivedToggleOption>('teaching');
 
+  const sections = useAppSelector(state => state.teacherSections.sections);
+
+  // The server uses hidden to mean the same thing as archived.
+  const showHiddenOnly = selectedArchiveToggle === 'archived';
+
+  const numSections = React.useMemo(
+    () =>
+      Object.values(sections).filter(
+        section => showHiddenOnly === section.hidden
+      ).length,
+    [sections, showHiddenOnly]
+  );
+
   return (
     <div className={styles.teacherHomepage}>
       <div className={styles.teacherHomepageBody}>
@@ -36,9 +50,13 @@ export const TeacherHomepage: React.FC = () => {
               selectedArchiveToggle={selectedArchiveToggle}
               setSelectedArchiveToggle={setSelectedArchiveToggle}
             />
-            <SectionList
-              showHiddenOnly={selectedArchiveToggle === 'archived'}
-            />
+            {numSections === 0 ? (
+              <EmptyHomepage showHiddenOnly={showHiddenOnly} />
+            ) : (
+              <SectionList
+                showHiddenOnly={selectedArchiveToggle === 'archived'}
+              />
+            )}
           </div>
           <div className={styles.blankAnnouncement} />
         </div>
