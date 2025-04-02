f01f9672558659e3ef7c5b48ac6195ad1f602930
Turner Riley <turner.riley@code.org>
2025-04-02 16:59:16 -0400

    fetch regional partner and workshop data from backend

--- apps/src/code-studio/pd/professional_learning/RegionalWorkshopCatalog.jsx
+++ apps/src/code-studio/pd/professional_learning/RegionalWorkshopCatalog.jsx
@@ -8,10 +8,45 @@ import {
 } from '@code-dot-org/component-library/typography';
 import React, {useState} from 'react';
 
+import {getAuthenticityToken} from '@cdo/apps/util/AuthenticityTokenStore';
+
 import style from './regionalWorkshopCatalog.module.scss';
 
 export default function RegionalWorkshopCatalog() {
   const [zipCode, setZipCode] = useState('');
+  const [regionalPartner, setRegionalPartner] = useState('');
+  const [availableWorkshops, setAvailableWorkshops] = useState([]);
+
+  const handleZipChage = async zip => {
+    setZipCode(zip);
+
+    if (zip.length === 5) {
+      try {
+        const response = await fetch(
+          '/dashboardapi/v1/pd/regional_workshop_data',
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-Token': await getAuthenticityToken(),
+            },
+            body: JSON.stringify({zip_code: zip}),
+          }
+        );
+
+        if (response.ok) {
+          const jsonData = await response.json();
+          setRegionalPartner(jsonData.regional_partner);
+          setAvailableWorkshops(jsonData.available_workshops);
+        }
+      } catch (error) {
+        console.error(
+          'Error fetching regional partner and available workshops:',
+          error
+        );
+      }
+    }
+  };
 
   return (
     <div className={style.workshopCatalog}>
@@ -29,7 +64,7 @@ export default function RegionalWorkshopCatalog() {
               id="zipCode"
               name="zipCode"
               label="School ZIP Code:"
-              onChange={e => setZipCode(e.target.value)}
+              onChange={e => handleZipChage(e.target.value)}
               value={zipCode}
               maxLength={255}
               placeholder="12345"
@@ -41,9 +76,15 @@ export default function RegionalWorkshopCatalog() {
               Your Regional Partner
             </OverlineTwoText>
             <div className={style.rpInfo}>
-              <BodyTwoText className={style.rpName}>
-                Sample Regional Partner Name
-              </BodyTwoText>
+              {regionalPartner ? (
+                <BodyTwoText className={style.rpName}>
+                  {regionalPartner}
+                </BodyTwoText>
+              ) : (
+                <BodyTwoText className={style.rpNameMissing}>
+                  Zip code required
+                </BodyTwoText>
+              )}
               <div className={style.rpInfoButtons}>
                 <LinkButton
                   color="black"
@@ -72,6 +113,13 @@ export default function RegionalWorkshopCatalog() {
             are looking for check back again soon or{' '}
             <a href="/">contact your regional partner</a>.
           </BodyTwoText>
+        </div>
+        <div>
+          <ul>
+            {availableWorkshops.map(workshop => (
+              <li>{workshop.id}</li>
+            ))}
+          </ul>
         </div>
       </section>
     </div>
