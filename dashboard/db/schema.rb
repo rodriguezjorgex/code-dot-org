25104a447dd23960a9360b2820eea29744146d15
Erin Bond <erin.bond@code.org>
2025-04-02 14:48:27 -0400

    schema format

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2025_04_01_152234) do
+
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "level_id"
