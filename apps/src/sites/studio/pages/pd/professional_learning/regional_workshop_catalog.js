6c201e869fdfc642808b2493ead74385dc718952
Turner Riley <turner.riley@code.org>
2025-04-02 14:02:30 -0400

    remove unused store

--- apps/src/sites/studio/pages/pd/professional_learning/regional_workshop_catalog.js
+++ apps/src/sites/studio/pages/pd/professional_learning/regional_workshop_catalog.js
@@ -1,18 +1,12 @@
 import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
 
 import RegionalWorkshopCatalog from '@cdo/apps/code-studio/pd/professional_learning/RegionalWorkshopCatalog';
-import {getStore} from '@cdo/apps/redux';
 
 $(() => {
-  const store = getStore();
-
   ReactDOM.render(
-    <Provider store={store}>
-      <RegionalWorkshopCatalog />
-    </Provider>,
+    <RegionalWorkshopCatalog />,
     document.getElementById('regional-workshop-catalog')
   );
 });
