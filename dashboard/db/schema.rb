b46b64582967286463b7d07777df388775d0e831
Erin Bond <erin.bond@code.org>
2025-04-02 10:47:45 -0400

    fix schema version, take current

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2025_04_01_152234) do
-=======
-ActiveRecord::Schema.define(version: 2025_03_21_190657) do
->>>>>>> staging
-
   create_table "activities", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "level_id"
