a0a7b4987d3360b8310441121dcee98b4bccee24
denyslevada <levada.denys@gmail.com>
2025-04-14 17:28:28 +0300

    feat(heroBanner): add HeroBanner component definition

---
+++ frontend/apps/marketing/src/components/contentful/heroBanner/HeroBanner.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import React from 'react';
+
+import DSCOHeroBanner from '@code-dot-org/component-library/cms/heroBanner';
+import {Theme} from '@code-dot-org/component-library/common/contexts';
+
+import {externalLinkIconProps} from '@/components/common/constants';
+
+type HeroBannerProps = {
+  /** HeroBanner content mode (theme) value */
+  contentMode: Theme;
+  /** HeroBanner image size (whether show with wide text (text is wider than image)) */
+  imageSize: 'Big' | 'Large';
+  /** Whether to show the background color */
+  removeBackground?: boolean;
+  /** HeroBanner cf styles*/
+  className?: string;
+  /** HeroBanner heading */
+  heading: string;
+  /** HeroBanner subHeading */
+  subHeading?: string;
+  /** HeroBanner description */
+  description?: string;
+  /** Section Image URL */
+  sectionImage?: string;
+  // sectionVideo?: ExperienceAsset;
+  /** HeroBanner button label */
+  buttonLabel?: string;
+  /** HeroBanner button URL */
+  buttonUrl?: string;
+  /** HeroBanner button is external link */
+  buttonIsLinkExternal?: boolean;
+  /** HeroBanner button aria label */
+  buttonAriaLabel?: string;
+  /** HeroBanner partner image URL */
+  partnerLogo?: string;
+  /** HeroBanner partner callout (title) */
+  partnerCallout?: string;
+  /** HeroBanner background image URL */
+  backgroundImage?: string;
+};
+
+const HeroBanner: React.FunctionComponent<HeroBannerProps> = ({
+  // Style Props
+  contentMode,
+  imageSize,
+  // Content Props
+  heading,
+  subHeading,
+  description,
+  sectionImage,
+  // sectionVideo,
+  buttonLabel,
+  buttonUrl,
+  buttonIsLinkExternal,
+  buttonAriaLabel,
+  partnerLogo,
+  partnerCallout,
+  backgroundImage,
+  removeBackground = false,
+  className,
+}) => {
+  return (
+    <DSCOHeroBanner
+      data-theme={contentMode}
+      withWideText={imageSize === 'Big'}
+      className={className}
+      heading={heading}
+      subHeading={subHeading}
+      description={description}
+      imageProps={sectionImage ? {src: sectionImage} : undefined}
+      buttonProps={{
+        text: buttonLabel,
+        href: buttonUrl,
+        ariaLabel: buttonAriaLabel,
+        iconRight: buttonIsLinkExternal ? externalLinkIconProps : undefined,
+      }}
+      partner={
+        partnerLogo && partnerCallout
+          ? {title: partnerCallout, logo: {src: partnerLogo}}
+          : undefined
+      }
+      backgroundImageUrl={backgroundImage}
+      // videoProps={videoProps}
+      removeBackground={removeBackground}
+    />
+  );
+};
+
+export default HeroBanner;
