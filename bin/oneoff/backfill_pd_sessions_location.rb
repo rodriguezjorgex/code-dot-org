d8baf175cfc22ed5bb4205aae2b71cf74cf00231
Ryan Driscoll <driscollrp@gmail.com>
2025-04-21 11:28:37 -0600

    chore: add oneoff script to update session location data with workshop location data

--- bin/oneoff/backfill_pd_sessions_location.rb
+++ bin/oneoff/backfill_pd_sessions_location.rb
@@ -1,21 +1,28 @@
 #!/usr/bin/env ruby
 
+# As of April 10, 2025:
+# The total number of sessions with available workshop location data is 19_686
+# The sessions table itself only has ~ 150 more sessions, so I decided not to join
+# with the workshops table to filter to just workshops with location data.
+
 require_relative '../../dashboard/config/environment'
+
+count = 0
 
 Pd::Session.
   includes(:workshop).
-  where(
-    'pd_workshops.location_name IS NOT NULL OR pd_workshops.location_address IS NOT NULL'
-  ).
   find_each(batch_size: 500) do |session|
     workshop = session.workshop
     next unless workshop
 
-    # Backfill location_name and location_address
     session.location_name = workshop.location_name
     session.location_address = workshop.location_address
 
-    # Save only if changes were made
-    # session.save! if session.changed?
-    puts "update session #{session.id} with location_name: #{workshop.location_name}, location_address: #{workshop.location_address}" if session.changed?
+    if session.changed?
+      puts "Updating session #{session.id} with new location data"
+      session.save!
+      count += 1
+    end
   end
+
+puts "#{count} sessions updated with workshop location"
