b6881e967568c5416d70ffe42346b576a18e1519
Mike Harvey <mike.harvey@code.org>
2025-04-24 17:14:27 -0400

    feature: support rendering all music and sprite lab blocks in documentation, including navigation bar

--- apps/src/templates/utils/embeddedBlocklyUtils.js
+++ apps/src/templates/utils/embeddedBlocklyUtils.js
@@ -1,15 +1,29 @@
 import {installCustomBlocks} from '@cdo/apps/block_utils';
+import {PROCEDURE_BLOCK_TYPES} from '@cdo/apps/blockly/constants';
+import commonBlocks from '@cdo/apps/blocksCommon';
 import assetUrl from '@cdo/apps/code-studio/assetUrl';
 import {getStore} from '@cdo/apps/code-studio/redux';
+import {installFunctionBlocks} from '@cdo/apps/music/blockly/blockUtils';
+import {setUpBlocklyForMusicLab} from '@cdo/apps/music/blockly/setup';
 import animationList, {
   setInitialAnimationList,
 } from '@cdo/apps/p5lab/redux/animationList';
-import {customInputTypes} from '@cdo/apps/p5lab/spritelab/blocks';
+import {
+  customInputTypes,
+  install as installSpriteLabBlocks,
+} from '@cdo/apps/p5lab/spritelab/blocks';
 import {
   valueTypeTabShapeMap,
   exampleSprites,
 } from '@cdo/apps/p5lab/spritelab/constants';
 import {registerReducers} from '@cdo/apps/redux';
+import skinBase from '@cdo/apps/skins';
+import {parseElement} from '@cdo/apps/xml';
+
+import {
+  convertXmlToBlockly,
+  shrinkBlockSpaceContainer,
+} from '../instructions/utils';
 
 /**
  * Prepares the blockly environment to allow for embedding blocks in divs
@@ -17,8 +31,23 @@ import {registerReducers} from '@cdo/apps/redux';
  *
  * @param {object[]} list of blocks that can be embedded
  */
-export const prepareBlocklyForEmbedding = function (customBlocksConfig) {
-  if (!customBlocksConfig) {
+export const prepareBlocklyForEmbedding = function (
+  customBlocksConfig,
+  programmingEnvironmentName
+) {
+  if (['spritelab', 'music'].includes(programmingEnvironmentName)) {
+    // Shareable procedures blocks are used in both Sprite Lab and Music Lab.
+    installFunctionBlocks();
+    if (programmingEnvironmentName === 'spritelab') {
+      installSpriteLabBlocks(Blockly);
+    }
+    if (programmingEnvironmentName === 'music') {
+      // Music Lab does not use block pools, so we need to install blocks in a different way.
+      setUpBlocklyForMusicLab();
+      return;
+    }
+  } else if (!customBlocksConfig) {
+    // Skip Blockly setup for other labs that don't use block pools.
     return;
   }
   Blockly.assetUrl = assetUrl;
@@ -33,9 +62,48 @@ export const prepareBlocklyForEmbedding = function (customBlocksConfig) {
   store.dispatch(setInitialAnimationList(exampleSprites));
   Blockly.valueTypeTabShapeMap = valueTypeTabShapeMap(Blockly);
 
+  // Install common blocks like repeat loops and "when run"
+  commonBlocks.install(Blockly, {
+    skin: skinBase.load(Blockly.assetUrl, programmingEnvironmentName),
+    isK1: false,
+  });
+
+  // Install custom blocks, ie. those defined in the block pool
   installCustomBlocks({
     blockly: Blockly,
     blockDefinitions: customBlocksConfig,
     customInputTypes,
   });
 };
+
+/**
+ * Creates and renders an embedded Blockly block into the given container element.
+ * @param {HTMLElement} container - The DOM node to render into
+ * @param {string} blockType - The Blockly block type
+ */
+export function embedBlocklyBlock(container, blockType) {
+  convertXmlToBlockly(container);
+  const blocksDom = parseElement(`<block type='${blockType}' />`);
+  addNameToProcedureBlocks(blocksDom);
+  const blockSpace = Blockly.createEmbeddedWorkspace(container, blocksDom, {
+    noScrolling: true,
+    inline: true,
+  });
+  shrinkBlockSpaceContainer(blockSpace, true);
+}
+
+/**
+ * Modifies the given block DOM element to add a default procedure name if it's a procedure block.
+ *
+ * @param {Element} blocksDom - An XML DOM element containing a single <block> node.
+ * @private
+ */
+function addNameToProcedureBlocks(blocksDom) {
+  const blockElement = blocksDom.querySelector('block');
+  if (PROCEDURE_BLOCK_TYPES.includes(blockElement.getAttribute('type'))) {
+    const fieldElement = document.createElement('field');
+    fieldElement.setAttribute('name', 'NAME');
+    fieldElement.textContent = Blockly.Msg.PROCEDURES_DEFNORETURN_PROCEDURE;
+    blockElement.appendChild(fieldElement);
+  }
+}
