ed20ae85aa293667831dfd5984b3a4760b7b2c21
Molly Moen <molly@code.org>
2025-05-06 15:39:21 -0700

    remove logs

--- dashboard/app/controllers/script_levels_controller.rb
+++ dashboard/app/controllers/script_levels_controller.rb
@@ -203,8 +203,6 @@ class ScriptLevelsController < ApplicationController
       end
     end
 
-    puts "body_classes: #{@body_classes}"
-
     @rubric = @script_level.lesson.rubric
     ai_rubrics_enabled_for_user = @view_as_user&.verified_teacher? || @view_as_user&.teachers&.any?(&:verified_teacher?)
     if @rubric && ai_rubrics_enabled_for_user
