c6d10e8a37f0747ec5a1e1ce93f8730309f7393a
Molly Moen <molly@code.org>
2025-05-22 14:19:03 -0700

    update picker to theme correctly

--- apps/src/pythonlab/components/ProjectTypePicker.tsx
+++ apps/src/pythonlab/components/ProjectTypePicker.tsx
@@ -1,4 +1,5 @@
 import ActionBlock from '@code-dot-org/component-library/actionBlock';
+import {useTheme} from '@code-dot-org/component-library/common/contexts';
 import {CustomDialog} from '@code-dot-org/component-library/dialog';
 import {
   BodyThreeText,
@@ -25,10 +26,12 @@ const ProjectTypePicker: React.FunctionComponent<ProjectTypePickerProps> = ({
 }) => {
   const isNeighborhood = currentProjectType === 'neighborhood';
   const isConsole = currentProjectType === 'console';
+  const {theme} = useTheme();
+  const mode = theme === 'Light' ? 'light' : 'dark';
   return (
-    <div className={moduleStyles.dialogContainer} data-theme="Dark">
+    <div className={moduleStyles.dialogContainer}>
       <CustomDialog
-        mode="dark"
+        mode={mode}
         className={moduleStyles.pickerDialog}
         aria-labelledby="project-picker-title"
         onClose={currentProjectType ? closeDialog : undefined}
