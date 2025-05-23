d73e474331dc7e316886067322ab2b5a56d30a2a
Turner Riley <turner.riley@code.org>
2025-05-23 14:42:21 -0400

    move into function

--- dashboard/app/controllers/pd/professional_learning_controller.rb
+++ dashboard/app/controllers/pd/professional_learning_controller.rb
@@ -164,8 +164,11 @@ class Pd::ProfessionalLearningController < ApplicationController
     workshops = (rp_workshops + national_workshops).uniq(&:id)
 
     available_workshops = workshops.select do |ws|
+      start_of_ws = ws.sessions.first.try(:start)
+      start_date = ws.time_zone ? start_of_ws.in_time_zone(ws.time_zone).to_date : start_of_ws.to_date
+
       ws.state == Pd::Workshop::STATE_NOT_STARTED &&
-        get_workshop_start_date(ws) > Time.now.in_time_zone(ws.time_zone || 'America/Chicago').to_date &&
+        start_date > Time.now.in_time_zone(ws.time_zone || 'America/Chicago').to_date &&
         !ws.hidden &&
         in_region?(ws, partner) &&
         has_allowed_course_for_regional_ws_page?(ws)
@@ -182,11 +185,6 @@ class Pd::ProfessionalLearningController < ApplicationController
   # Returns if the current_user can view the facilitator landing page of the given course.
   private def can_view_facilitator_page(course)
     current_user&.can_view_all_facilitator_landing_pages? || current_user&.courses_as_facilitator&.exists?(course: course)
-  end
-
-  private def get_workshop_start_date(workshop)
-    start_of_ws = workshop.sessions.first.try(:start)
-    workshop.time_zone ? start_of_ws.in_time_zone(workshop.time_zone).to_date : start_of_ws.to_date
   end
 
   # Returns if the given workshop is within the provided regional partner's area (including
