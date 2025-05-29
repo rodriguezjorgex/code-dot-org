f0343b0d579388d6d060aea4c2ec7362078fb234
denyslevada <levada.denys@gmail.com>
2025-05-29 18:54:10 +0300

    feat(enrollInWorkshop): refactor enrollment action to use LinkButton and improve logic for full workshops

--- apps/src/code-studio/pd/workshops/components/EnrollInWorkshop.tsx
+++ apps/src/code-studio/pd/workshops/components/EnrollInWorkshop.tsx
@@ -1,4 +1,4 @@
-import Button from '@code-dot-org/component-library/button';
+import Button, {LinkButton} from '@code-dot-org/component-library/button';
 import Link from '@code-dot-org/component-library/link';
 import {
   Heading3,
@@ -6,36 +6,72 @@ import {
 } from '@code-dot-org/component-library/typography';
 import React from 'react';
 
+import {GetWorkshopInfoScriptDataResponse} from '@cdo/apps/code-studio/pd/workshops/types';
+
 import moduleStyles from './../workshopMarketingPage.module.scss';
 
-type EnrollInWorkshopProps = {
-  custom_registration_link?: string;
-};
+interface EnrollInWorkshopProps
+  extends Pick<
+    GetWorkshopInfoScriptDataResponse,
+    'custom_registration_link' | 'num_enrollments' | 'capacity' | 'id'
+  > {}
 
 /** Component to display the enrollment information for a workshop. */
 const EnrollInWorkshop: React.FC<EnrollInWorkshopProps> = ({
+  id,
   custom_registration_link,
+  num_enrollments,
+  capacity,
 }) => {
-  return (
-    <div className={moduleStyles.card}>
-      <Heading3 visualAppearance="heading-xs">Enroll in this workshop</Heading3>
-      <BodyThreeText>
-        This workshop’s registration is managed externally by the regional
-        partner.
-      </BodyThreeText>
-      {custom_registration_link ? (
-        <a href={custom_registration_link} className={moduleStyles.cta}>
-          Go to partner enrollment
-        </a>
-      ) : (
+  const isFull = num_enrollments >= capacity;
+
+  const renderEnrollmentAction = () => {
+    if (isFull) {
+      return (
         <Button
           className={moduleStyles.fullWidthButton}
           size="m"
           disabled
+          text="Workshop is full"
           onClick={() => null}
-          text="Enrollment Closed"
         />
-      )}
+      );
+    }
+
+    if (custom_registration_link) {
+      return (
+        <>
+          <BodyThreeText>
+            This workshop’s registration is managed externally by the regional
+            partner.
+          </BodyThreeText>
+          <LinkButton
+            href={custom_registration_link}
+            className={moduleStyles.fullWidthButton}
+            type="primary"
+            size="m"
+            text="Go to partner enrollment"
+            iconRight={{iconName: 'up-right-from-square'}}
+          />
+        </>
+      );
+    }
+
+    return (
+      <LinkButton
+        className={moduleStyles.fullWidthButton}
+        type="primary"
+        size="m"
+        href={`/pd/workshops/${id}/enroll`}
+        text="Enroll in this workshop"
+      />
+    );
+  };
+
+  return (
+    <div className={moduleStyles.card}>
+      <Heading3 visualAppearance="heading-xs">Enroll in this workshop</Heading3>
+      {renderEnrollmentAction()}
       <Link type="secondary" size="xs" href="#data-sharing-notice">
         Click to see data sharing notice
       </Link>
