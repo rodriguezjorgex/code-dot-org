2d3db2e79f0d34074ac47392cde3dddc3ab68389
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-06-02 16:05:45 -0700

    fix: add additional error details
    When an error occurs, the browser simply logs out "Error" in Playwright's tracer. This logs out the error with its message and stack at the INFO level in case the Error is not serialized.

--- frontend/apps/marketing/src/otel/errorHandler.ts
+++ frontend/apps/marketing/src/otel/errorHandler.ts
@@ -1,6 +1,8 @@
 import NewRelicAgent from '@/providers/newrelic/agent';
 export function handleError(error: Error, errorTraceId: string) {
   console.error(error, errorTraceId);
+  console.log(`Error ${errorTraceId} received ${error.message}`);
+  console.log(`Error ${errorTraceId} stack ${error.stack}`);
 
   NewRelicAgent.then(agent => {
     if (agent) {
