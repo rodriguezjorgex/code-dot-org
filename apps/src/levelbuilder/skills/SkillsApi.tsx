1cce963c8484cd65c64da7c47361ff02178052f4
Erin Bond <erin.bond@code.org>
2025-06-04 11:38:20 -0400

    add LevelsSkills controller + API

--- apps/src/levelbuilder/skills/SkillsApi.tsx
+++ apps/src/levelbuilder/skills/SkillsApi.tsx
@@ -33,9 +33,44 @@ export async function createSkill(skill: Skill) {
     return data;
   } catch (error) {
     MetricsReporter.logError({
-      event: MetricEvent.AI_TUTOR_CHAT_SAVE_FAIL,
+      event: MetricEvent.SKILL_SAVE_FAIL,
       errorMessage:
-        (error as Error).message || 'Failed to save AI Tutor interaction',
+        (error as Error).message || 'Failed to save Skill',
     });
   }
+
+  interface LevelsSkill {
+    skillId: number;
+    levelId: number;
+  }
+
+  export async function createLevelsSkill(levelsSkill: LevelsSkill) {
+    try {
+      const response = await fetch(`/levels_skills`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': await getAuthenticityToken(),
+        },
+        body: JSON.stringify(skill),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        const msg = `Network response was not ok creating LevelsSkill ${JSON.stringify(
+          errorData,
+          null,
+          2
+        )}`;
+        console.error(msg);
+        return null;
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      MetricsReporter.logError({
+        event: MetricEvent.LEVELS_SKILL_SAVE_FAIL,
+        errorMessage:
+          (error as Error).message || 'Failed to save LevelsSkill',
+      });
+    }
 }
