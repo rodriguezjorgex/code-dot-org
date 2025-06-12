411bf936cb2f61398e398c5011c51bb6e226af7e
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-06-12 14:35:20 -0700

    404 page should not be english only

--- frontend/apps/marketing/src/app/[brand]/[locale]/not-found.tsx
+++ frontend/apps/marketing/src/app/[brand]/[locale]/not-found.tsx
@@ -19,13 +19,13 @@ export default function NotFound() {
   }, [pathname]);
 
   return (
-    <html lang="en">
+    <>
       {/*
         No support for metadata in not-found.tsx yet
         See: https://github.com/vercel/next.js/issues/45620
       */}
       <title>Page Not Found</title>
       <Error statusCode={404} />
-    </html>
+    </>
   );
 }
