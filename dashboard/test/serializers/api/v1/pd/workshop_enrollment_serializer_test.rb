f08faffabc7bdac63188c24afc50d96d65a8f107
Turner Riley <turner.riley@code.org>
2025-07-15 13:15:09 -0400

    remove user info as not required on backend

--- dashboard/test/serializers/api/v1/pd/workshop_enrollment_serializer_test.rb
+++ dashboard/test/serializers/api/v1/pd/workshop_enrollment_serializer_test.rb
@@ -21,7 +21,6 @@ class Api::V1::Pd::WorkshopEnrollmentSerializerTest < ActionController::TestCase
 
   test 'attendances' do
     workshop = create :workshop, num_sessions: 5
-    user = create :teacher, given_name: 'Firstname', family_name: 'Lastname', school_info: (create :school_info)
     enrollment = create :pd_enrollment, workshop: workshop, user: user
     create :pd_attendance, session: workshop.sessions.first, enrollment: enrollment
 
