9f3a7186b733cf33fc9dafd6b41702286f638b02
Kelby Hawn <9256643+kelbyhawn@users.noreply.github.com>
2025-07-21 11:29:38 -0700

    feat(marketing): refactor Overline component to use MUI (#67231)
    * feat(marketing): update Overline component to use MUI
    
    * update tests
    
    * pr feedback from Copilot and clean up
    
    * remove brackets
    
    * remove brackets

--- frontend/apps/marketing/src/themes/csforall/index.ts
+++ frontend/apps/marketing/src/themes/csforall/index.ts
@@ -1,5 +1,6 @@
 'use client';
 import {createTheme} from '@mui/material';
+import {alpha} from '@mui/material/styles';
 
 import {NOTO_FONT} from '@/themes/constants/fonts';
 
@@ -34,44 +35,44 @@ const theme = createTheme({
       styleOverrides: {
         root: ({theme}) => ({
           fontFamily: ROBOTO_MONO_FONT,
-          ['&:focus-visible']: {
+          '&:focus-visible': {
             outline: '2px solid ' + theme.palette.primary.main,
             outlineOffset: '2px',
           },
           textTransform: 'none',
           border: '1px solid transparent',
-          ['&.MuiButton-contained.button--color-emphasized']: {
+          '&.MuiButton-contained.button--color-emphasized': {
             backgroundColor: theme.palette.secondary.main,
-            ['&:hover']: {
+            '&:hover': {
               backgroundColor: theme.palette.secondary.dark,
             },
           },
-          ['&.MuiButton-contained.button--color-primary']: {
+          '&.MuiButton-contained.button--color-primary': {
             backgroundColor: theme.palette.primary.main,
-            ['&:hover']: {
+            '&:hover': {
               backgroundColor: theme.palette.primary.dark,
             },
           },
-          ['&.MuiButton-outlined.button--color-secondary']: {
+          '&.MuiButton-outlined.button--color-secondary': {
             color: theme.palette.common.black,
             borderColor: theme.palette.common.black,
-            ['&:hover']: {
+            '&:hover': {
               backgroundColor: theme.palette.grey[100],
             },
           },
-          ['&.MuiButton-contained.MuiButton-sizeSmall, &.MuiButton-outlined.MuiButton-sizeSmall']:
+          '&.MuiButton-contained.MuiButton-sizeSmall, &.MuiButton-outlined.MuiButton-sizeSmall':
             {
               fontSize: '1rem',
               padding: theme.spacing(1.25, 2.5),
               borderRadius: theme.spacing(3),
             },
-          ['&.MuiButton-contained.MuiButton-sizeMedium, &.MuiButton-outlined.MuiButton-sizeMedium']:
+          '&.MuiButton-contained.MuiButton-sizeMedium, &.MuiButton-outlined.MuiButton-sizeMedium':
             {
               fontSize: '1.125rem',
               padding: theme.spacing(1.5, 3),
               borderRadius: theme.spacing(4),
             },
-          ['&.MuiButton-contained.MuiButton-sizeLarge, &.MuiButton-outlined.MuiButton-sizeLarge']:
+          '&.MuiButton-contained.MuiButton-sizeLarge, &.MuiButton-outlined.MuiButton-sizeLarge':
             {
               fontSize: '1.25rem',
               padding: theme.spacing(2, 5),
@@ -83,30 +84,30 @@ const theme = createTheme({
     MuiDivider: {
       styleOverrides: {
         root: ({theme}) => ({
-          ['&.MuiDivider-root.divider--color-primary']: {
+          '&.MuiDivider-root.divider--color-primary': {
             borderColor: theme.palette.divider,
           },
-          ['&.MuiDivider-root.divider--color-strong']: {
+          '&.MuiDivider-root.divider--color-strong': {
             borderColor: theme.palette.divider,
             borderStyle: 'dashed',
           },
-          ['&.MuiDivider-root.divider--margin-none']: {
+          '&.MuiDivider-root.divider--margin-none': {
             marginTop: 0,
             marginBottom: 0,
           },
-          ['&.MuiDivider-root.divider--margin-xs']: {
+          '&.MuiDivider-root.divider--margin-xs': {
             marginTop: theme.spacing(3),
             marginBottom: theme.spacing(3),
           },
-          ['&.MuiDivider-root.divider--margin-s']: {
+          '&.MuiDivider-root.divider--margin-s': {
             marginTop: theme.spacing(5),
             marginBottom: theme.spacing(5),
           },
-          ['&.MuiDivider-root.divider--margin-m']: {
+          '&.MuiDivider-root.divider--margin-m': {
             marginTop: theme.spacing(7),
             marginBottom: theme.spacing(7),
           },
-          ['&.MuiDivider-root.divider--margin-l']: {
+          '&.MuiDivider-root.divider--margin-l': {
             marginTop: theme.spacing(8),
             marginBottom: theme.spacing(8),
           },
@@ -117,6 +118,37 @@ const theme = createTheme({
       styleOverrides: {
         root: ({theme}) => ({
           color: theme.palette.text.primary,
+          // Overline styles
+          '&.MuiTypography-overline': {
+            display: 'inline-block',
+            paddingBlock: theme.spacing(0.5), // 4px
+            paddingInline: theme.spacing(1), // 8px
+            borderRadius: 8,
+          },
+          '&.MuiTypography-overline.overline--color-primary': {
+            color: theme.palette.primary.main,
+            border: `1px solid ${theme.palette.primary.main}`,
+          },
+          '&.MuiTypography-overline.overline--color-secondary': {
+            color: theme.palette.secondary.dark,
+            backgroundColor: alpha(theme.palette.secondary.main, 0.1),
+            border: `1px solid ${theme.palette.secondary.dark}`,
+          },
+          '&.MuiTypography-overline.overline--size-s': {
+            fontSize: '0.75rem', // 12px
+          },
+          '&.MuiTypography-overline.overline--size-m': {
+            fontSize: '0.875rem', // 14px
+          },
+          '&.MuiTypography-overline.overline--size-l': {
+            fontSize: '1rem', // 16px
+          },
+          // End Overline styles
+        }),
+        gutterBottom: ({theme}) => ({
+          '&.MuiTypography-overline': {
+            marginBottom: theme.spacing(2), // 16px
+          },
         }),
       },
     },
@@ -158,6 +190,11 @@ const theme = createTheme({
       fontSize: '1.5rem', // 24px
       fontWeight: 800,
       lineHeight: 1.25,
+    },
+    overline: {
+      fontWeight: 400,
+      lineHeight: 1.5,
+      textTransform: 'none',
     },
   },
 });
