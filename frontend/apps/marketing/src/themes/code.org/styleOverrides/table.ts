d564fab7f9a4ef7d9f026ea95376a79e3d16b0ee
denyslevada <levada.denys@gmail.com>
2025-08-04 21:34:13 +0300

    style(Table): update styles to use CSS variables for consistency

--- frontend/apps/marketing/src/themes/code.org/styleOverrides/table.ts
+++ frontend/apps/marketing/src/themes/code.org/styleOverrides/table.ts
@@ -10,30 +10,30 @@ export const TABLE_CELL_OVERRIDES: Components<Theme>['MuiTableCell'] = {
   styleOverrides: {
     root: ({theme}) => ({
       padding: `${theme.spacing(1.75)} ${theme.spacing(2.25)}`, //14px 18px
-      border: `1px solid ${theme.palette.grey['200']}`,
+      border: `1px solid var(--borders-neutral-primary)`,
     }),
-    head: ({theme}) => ({
+    head: () => ({
       fontSize: '0.875rem',
       fontWeight: 500,
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.text.secondary,
-      borderColor: theme.palette.primary.main,
+      backgroundColor: 'var(--background-brand-teal-primary)',
+      color: 'var(--text-neutral-white-fixed);',
+      borderColor: 'var(--borders-brand-teal-strong)',
       textTransform: 'uppercase',
     }),
-    body: ({theme}) => ({
+    body: () => ({
       fontSize: '1.125rem',
-      color: theme.palette.text.primary,
-      border: `1px solid ${theme.palette.grey['200']}`,
+      color: 'var(--text-neutral-primary)',
+      border: `1px solid var(--borders-neutral-primary)`,
     }),
   },
 };
 
 export const TABLE_ROW_OVERRIDES: Components<Theme>['MuiTableRow'] = {
   styleOverrides: {
-    root: ({theme}) => ({
-      backgroundColor: theme.palette.background.default,
+    root: () => ({
+      backgroundColor: 'var(--background-neutral-secondary);',
       '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.background.paper,
+        backgroundColor: 'var(--background-neutral-primary)',
       },
     }),
   },
