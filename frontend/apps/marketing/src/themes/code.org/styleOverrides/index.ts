3d54efecb7a2d5b11859f5b6407253d8f1dd2ef3
denyslevada <levada.denys@gmail.com>
2025-08-04 21:32:53 +0300

    style(SvgIcon): add style overrides for consistent icon color

--- frontend/apps/marketing/src/themes/code.org/styleOverrides/index.ts
+++ frontend/apps/marketing/src/themes/code.org/styleOverrides/index.ts
@@ -4,6 +4,7 @@ import {CONTAINER_OVERRIDES} from './container';
 import {DIVIDER_OVERRIDES} from './divider';
 import {IMAGE_OVERRIDES} from './image';
 import {LINK_OVERRIDES} from './link';
+import {SVG_ICON_OVERRIDES} from './svgIcon';
 import {
   TABLE_OVERRIDES,
   TABLE_ROW_OVERRIDES,
@@ -15,6 +16,7 @@ import {TYPOGRAPHY_OVERRIDES} from './typography';
 export const STYLE_OVERRIDES: Components<Theme> = {
   MuiContainer: CONTAINER_OVERRIDES,
   MuiDivider: DIVIDER_OVERRIDES,
+  MuiSvgIcon: SVG_ICON_OVERRIDES,
   MuiImage: IMAGE_OVERRIDES,
   MuiLink: LINK_OVERRIDES,
   MuiTable: TABLE_OVERRIDES,
