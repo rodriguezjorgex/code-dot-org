13fdbde6bbd3a580355411bdb6b15c5ea4671a7e
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-08-05 13:23:10 -0700

    add rich text

--- frontend/apps/marketing-storybook/stories/IconHighlight.story.tsx
+++ frontend/apps/marketing-storybook/stories/IconHighlight.story.tsx
@@ -2,6 +2,8 @@ import IconHighlightContentful from '@/components/contentful/iconHighlight';
 import type {Meta, StoryObj} from '@storybook/nextjs-vite';
 import {expect} from 'storybook/test';
 
+import IconHighlightMock from './__mocks__/IconHighlight.json';
+
 const meta: Meta<typeof IconHighlightContentful> = {
   title: 'Marketing/IconHighlight',
   component: IconHighlightContentful,
@@ -26,56 +28,7 @@ export const Playground: Story = {
 };
 
 export const FilledOut: Story = {
-  args: {
-    heading: 'Icon Highlight Heading',
-    text: 'Icon Highlight\nMultiline Text',
-    linkEntries: [
-      {
-        contentTypeId: 'link',
-        metadata: {
-          tags: [],
-          concepts: [],
-        },
-        sys: {
-          space: {
-            sys: {
-              type: 'Link',
-              linkType: 'Space',
-              id: '90t6bu6vlf76',
-            },
-          },
-          id: '3IqvCruY4yXUCkvfArArsD',
-          type: 'Entry',
-          createdAt: '2025-05-22T21:12:49.939Z',
-          updatedAt: '2025-07-23T18:10:13.642Z',
-          environment: {
-            sys: {
-              id: 'master',
-              type: 'Link',
-              linkType: 'Environment',
-            },
-          },
-          publishedVersion: 58,
-          revision: 14,
-          contentType: {
-            sys: {
-              type: 'Link',
-              linkType: 'ContentType',
-              id: 'link',
-            },
-          },
-          locale: 'en-US',
-        },
-        fields: {
-          linkName: '❌ [ENG] Editorial Card Link',
-          label: 'Editorial Card Link',
-          primaryTarget: '/editorial-card-test',
-          isThisAnExternalLink: true,
-        },
-      },
-    ],
-    iconName: 'smile',
-  },
+  args: IconHighlightMock,
   play: async ({canvas}) => {
     // Check heading
     const heading = canvas.getByRole('heading', {
