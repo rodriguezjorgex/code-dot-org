af6b667bed5dd498d46dd2d85a1d2227fd8a5dee
denyslevada <levada.denys@gmail.com>
2025-08-08 10:41:27 +0300

    style(List): enhance list component with new font stack and layout adjustments

--- frontend/apps/marketing/src/themes/code.org/styleOverrides/list.ts
+++ frontend/apps/marketing/src/themes/code.org/styleOverrides/list.ts
@@ -1,6 +1,8 @@
 import {Components, Theme} from '@mui/material/styles';
 
 import {FIGTREE_FONT} from '@/themes/code.org/constants/fonts';
+import {createFontStack} from '@/themes/common/constants';
+import {NOTO_FONT} from '@/themes/constants/fonts';
 
 export const LIST_OVERRIDES: Components<Theme>['MuiList'] = {
   styleOverrides: {
@@ -8,6 +10,12 @@ export const LIST_OVERRIDES: Components<Theme>['MuiList'] = {
       padding: 0,
       paddingInlineStart: theme.spacing(2.5), // 20px
       gap: '0.5rem',
+      '&.MuiList-root': {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        margin: theme.spacing(2, 0, 0),
+      },
     }),
   },
 };
@@ -18,6 +26,11 @@ export const LIST_ITEM_OVERRIDES: Components<Theme>['MuiListItem'] = {
       display: 'list-item',
       padding: 0,
       gap: theme.spacing(1),
+      color: 'var(--text-neutral-primary)',
+      fontFamily: createFontStack(FIGTREE_FONT, NOTO_FONT),
+      fontWeight: 400,
+      fontStyle: 'normal',
+
       '&::marker': {
         color: 'var(--text-neutral-primary)',
         fontFamily: FIGTREE_FONT,
--- frontend/apps/marketing/src/themes/csforall/styleOverrides/list.ts
+++ frontend/apps/marketing/src/themes/csforall/styleOverrides/list.ts
@@ -1,6 +1,8 @@
 import {Components, Theme} from '@mui/material/styles';
 
+import {createFontStack} from '@/themes/common/constants';
 import {NOTO_FONT} from '@/themes/constants/fonts';
+import {ROBOTO_MONO_FONT} from '@/themes/csforall/constants/fonts';
 
 export const LIST_OVERRIDES: Components<Theme>['MuiList'] = {
   styleOverrides: {
@@ -8,6 +10,12 @@ export const LIST_OVERRIDES: Components<Theme>['MuiList'] = {
       padding: 0,
       paddingInlineStart: theme.spacing(2.5), // 20px
       gap: '0.5rem',
+      '&.MuiList-root': {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        margin: theme.spacing(2, 0, 0),
+      },
     }),
   },
 };
@@ -18,6 +26,11 @@ export const LIST_ITEM_OVERRIDES: Components<Theme>['MuiListItem'] = {
       display: 'list-item',
       padding: 0,
       gap: theme.spacing(1),
+      color: theme.palette.text.primary,
+      fontFamily: createFontStack(ROBOTO_MONO_FONT, NOTO_FONT),
+      fontWeight: 400,
+      fontStyle: 'normal',
+
       '&::marker': {
         color: theme.palette.text.primary,
         fontFamily: NOTO_FONT,
