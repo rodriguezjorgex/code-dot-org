166b902df1513753faca7d56a0e9967805149ce9
Sanchit Malhotra <85528507+sanchitmalhotra126@users.noreply.github.com>
2025-09-16 17:23:39 -0700

    Lab2 Dance: stop program when song ends (#68361)
    * Lab2 Dance: stop program when song ends
    
    * use server theme

--- apps/src/dance/lab2/views/DanceView.tsx
+++ apps/src/dance/lab2/views/DanceView.tsx
@@ -79,8 +79,6 @@ const BLOCKLY_DIV_ID = 'dance-blockly-div';
 registerReducers(reducers);
 
 const isToolboxMode = getAppOptionsEditBlocks() === TOOLBOX_BLOCKS;
-const aiGenerateMode = queryParams('ai-generate') === 'true';
-const usingMusicProject = queryParams('music-channel') || aiGenerateMode;
 
 /**
  * Renders the Lab2 version of Dance Lab. This separate container
@@ -112,12 +110,11 @@ const DanceView: React.FunctionComponent<{
   const [loadedMusicProject, setLoadedMusicProject] = useState(false);
   const [generatedAiDance, setGeneratedAiDance] = useState(false);
 
-  const {setTheme} = useTheme();
-  useEffect(() => {
-    if (aiGenerateMode) {
-      setTheme('Dark');
-    }
-  }, [setTheme]);
+  const aiGenerateMode =
+    levelProperties.aiCodeGenerate || queryParams('ai-generate') === 'true';
+  const usingMusicProject = queryParams('music-channel') || aiGenerateMode;
+
+  const {theme} = useTheme();
 
   const metadataToUse: SongMetadata | undefined = useMemo(() => {
     if (!musicProjectPlayer.current || !loadedMusicProject) {
@@ -313,13 +310,13 @@ const DanceView: React.FunctionComponent<{
 
     workspace.current = Blockly.inject(blocklyDiv, {
       toolbox: aiGenerateMode ? undefined : toolbox,
-      theme: aiGenerateMode ? cdoDark : cdoTheme,
+      theme: theme === 'Dark' ? cdoDark : cdoTheme,
       readOnly: readonlyWorkspace,
       editBlocks: getAppOptionsEditBlocks(),
     } as BlocklyOptions);
 
     return () => workspace.current?.dispose();
-  }, [dispatch, readonlyWorkspace, levelProperties]);
+  }, [dispatch, readonlyWorkspace, levelProperties, aiGenerateMode, theme]);
 
   useEffect(() => {
     if (!workspace.current) {
@@ -380,7 +377,7 @@ const DanceView: React.FunctionComponent<{
         )
         .then(() => setLoadedMusicProject(true));
     }
-  }, []);
+  }, [usingMusicProject, aiGenerateMode]);
 
   // Set up the ProgramExecutor
   useEffect(() => {
@@ -403,14 +400,14 @@ const DanceView: React.FunctionComponent<{
       onEventsChanged,
       playSound: musicProjectPlayer.current
         ? (_url, callback) => {
-            console.log('play called at ', Date.now());
-            musicProjectPlayer.current?.play();
+            musicProjectPlayer.current?.play(resetProgram);
             callback(true);
           }
         : undefined,
       stopSound: musicProjectPlayer.current
         ? () => musicProjectPlayer.current?.stop()
         : undefined,
+      onSoundEnded: resetProgram,
     });
 
     if (recordReplayLog) {
@@ -453,6 +450,7 @@ const DanceView: React.FunctionComponent<{
     updateSources,
     loadedMusicProject,
     levelProperties.sharedBlocks,
+    usingMusicProject,
   ]);
 
   const settings = useBlocklySettings();
